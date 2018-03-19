57cd921d0292547e7adcb4044bff6bbd7575667a
Kate <jp.deplaix@gmail.com>
2018-03-19 16:34:00 +0000

    Make each opam-diff image unique

--- src/opam_ops.ml
+++ src/opam_ops.ml
@@ -186,6 +186,9 @@ let packages_from_diff ?(default=["ocamlfind"]) {build_t;run_t;_} target =
                   sed -E -n -e '\''s,\+\+\+ b/packages/[^/]*/([^/]*)/.*,\1,p'\'' | \
                   sort -u' >> /root/opam-github-pr-diff|} @@
       run "chmod +x /root/opam-github-pr-diff" @@
+      run "# PR number %d" pr_num @@ (* NOTE: Differenciate each images to avoid
+                                        rebuilding everything if rebuild is
+                                        triggered *)
       entrypoint_exec ["/root/opam-github-pr-diff"]
     in
     Docker_build.run build_t ~pull:true ~hum:"opam-diff image" dfile >>= fun img ->
